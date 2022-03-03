32a109b4ff7803d2146011025cffee21c60376bf
Alcides Andrade <andrade.alcides.junior@gmail.com>
2022-03-04 00:15:08 +0900

    add constructor, `Insert` and `Meld` operations

--- heap.go
+++ heap.go
@@ -4,27 +4,101 @@ const DefaultErrorParameter = 1e-10
 
 type SoftHeap[T any] struct {
 	// error paremeter
-	e float32
+	e float64
+	// the heap keeps a doubly linked list
+	// of binary trees.
+	// the list is sorted by the rank of each tree
+	treeListHead *softHeapTree[T]
+	treeListTail *softHeapTree[T]
+	rank         int
 }
 
-func New[T any]() SoftHeap[T] {
-	return SoftHeap[T]{e: DefaultErrorParameter}
+func New[T any](intialKey int, initialValue T) SoftHeap[T] {
+	heap := SoftHeap[T]{e: DefaultErrorParameter}
+	node := newNode(&heap, nil, nil, nil)
+	node.addElement(intialKey, initialValue)
+	treeListHead := newTree(&heap, nil, nil, &node)
+	heap.treeListHead = &treeListHead
+	heap.rank = treeListHead.rank()
+	return heap
 }
 
-func NewWithErrorParam[T any](e float32) SoftHeap[T] {
-	return SoftHeap[T]{e}
+func NewWithErrorParam[T any](e float64) SoftHeap[T] {
+	return SoftHeap[T]{e: e}
 }
 
 func (h *SoftHeap[T]) Insert(key int, value T) {
+	e := New(key, value)
+	h.Meld(&e)
 }
 
 func (h *SoftHeap[T]) Delete(key int) {
 }
 
-// `Meld` joins two soft heaps, returning the new larger heap.
-// The initial heaps are invalidated in the process.
-func (h *SoftHeap[T]) Meld(e *SoftHeap[T]) SoftHeap[T] {
-	return SoftHeap[T]{}
+// `Meld` joins two soft heaps. The new larger heap is stored
+// in `h`, while `i` is discarded.
+func (h *SoftHeap[T]) Meld(i *SoftHeap[T]) {
+	a := h // a references the heap with the smaller rank
+	b := i // b references the heap with the larger rank
+	if h.rank > i.rank {
+		a = i
+		b = h
+	}
+
+	at := a.treeListHead
+	bt := b.treeListHead
+	if at == nil && bt == nil {
+		return
+	}
+	min := func() (m *softHeapTree[T]) {
+		if at != nil && bt != nil {
+			if at.rank() <= bt.rank() {
+				m = at
+				at = at.next
+			} else {
+				m = bt
+				bt = bt.next
+			}
+		} else if at != nil {
+			m = at
+			at = at.next
+		} else {
+			m = bt
+			bt = bt.next
+		}
+		m.heap = h
+		return m
+	}
+
+	newListHead := min()
+	newListTail := newListHead
+	t := newListHead
+	// Merge the ordered list of `a` with the ordereed list of `b` (the order
+	// of the lists is by the rank of the root element).
+	for at != nil || bt != nil {
+		t.next = min()
+		t.next.prev = t
+		newListTail = t
+		t = t.next
+	}
+
+	t = newListHead
+	// Combine consecutive trees with the same rank, unless we have
+	// three consecutive trees with the same rank, in which case
+	// only the last two are combined.
+	for t.next != nil {
+		if t.rank() == t.next.rank() {
+			if t.next.next == nil || t.rank() != t.next.next.rank() {
+				t.combine()
+			}
+		}
+		t = t.next
+	}
+
+	h.treeListHead = newListHead
+	h.treeListTail = newListTail
+	h.rank = b.rank
+	h.treeListTail.updateSuffMin()
 }
 
 // `ExtractMin` returns the element with the smallest
