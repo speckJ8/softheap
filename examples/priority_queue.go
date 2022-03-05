b2688b61080fa1fcf0e6225d4dbd4176b60ea3e1
Alcides Andrade <andrade.alcides.junior@gmail.com>
2022-03-05 11:29:42 +0900

    priority queue example

--- examples/priority_queue.go
+++ examples/priority_queue.go
@@ -10,18 +10,18 @@ import (
 func main() {
 	// the priority of an element corresponds to
 	// the inverse of its key
-	heap := softheap.New[int]()
+	queue := softheap.New[int]()
 	for i := 0; i < 10; i++ {
 		// insert elements with random priorities
-		heap.Insert(rand.Intn(100), i)
+		queue.Insert(rand.Intn(100), i)
 	}
-	heap.Print()
+	queue.Print()
 	for {
-		k, e := heap.ExtractMin()
+		k, e := queue.ExtractMin()
 		if e == nil {
 			break
 		}
 		fmt.Printf("extrated priority=1/%d, value=%d\n", k, *e)
-		heap.Print()
+		queue.Print()
 	}
 }
